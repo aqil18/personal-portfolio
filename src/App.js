d0c8676cc7a69e62c3831c2d5a4bb60d6d8f7d8a
aqilf <aqilfaizal2004@gmail.com>
2024-08-13 14:09:52 -0700

    got it to work on my local

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Spline from '@splinetool/react-spline';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+       <Spline scene="https://prod.spline.design/obH5LwKOyhyvreZB/scene.splinecode" />
     </div>
   );
 }
